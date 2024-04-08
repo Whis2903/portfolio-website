cac87dbb78ddcb87e2f395b6f7997f4a0193e825
Vishal Mohan Nair <vishalmadridrox@gmail.com>
2024-04-08 13:58:39 +0530

    first commit

---
+++ src/lib/content/footer.ts
@@ -0,0 +1,6 @@
+import { FooterSectionType } from '@/lib/types/sections';
+
+export const footerSection: FooterSectionType = {
+    title: 'Design & Built by Vishal Mohan Nair',
+    link: 'https://github.com/vatsalsinghkv/portfolio-website',
+};
---
+++ src/lib/content/portfolio.ts
@@ -0,0 +1,38 @@
+/*
+ * =========================
+ * AUTHOR INFO
+ * =========================
+ */
+
+import { StringKeyValueType } from '../types';
+
+export const socialLinks: StringKeyValueType = {
+  facebook: 'https://www.facebook.com/vatsalsinghkv',
+  instagram: 'https://www.instagram.com/vatsalsinghkv',
+  github: 'https://github.com/Whis2903',
+  linkedin: 'https://www.linkedin.com/in/vishal-mohan-nair-6130a1177/',
+};
+
+export const author = {
+  name: 'Vishal Mohan Nair',
+  email: 'vishalmadridrox@gmail.com',
+};
+
+export const seoData = {
+  title: 'Vishal Mohan Nair | Front-End Developer',
+  description:
+    'Vishal Mohan Nair is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
+  author: author.name,
+  image:
+    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
+  url: 'https://vatsalsinghkv.vercel.app/',
+  keywords: [
+    'Vishal',
+    'Vishal Mohan Nair',
+    '@vatsalsinghkv',
+    'vatsalsinghkv',
+    'Portfolio',
+    'Vatsal Portfolio ',
+    'Vishal Mohan Nair Portfolio',
+  ],
+};
