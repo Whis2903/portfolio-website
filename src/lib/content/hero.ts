e95bd4df9f82e02c4a592aeb6b8e9a66ff8661db
Vishal Mohan Nair <112796398+Whis2903@users.noreply.github.com>
2024-05-03 13:08:23 +0530

    Update hero.ts

--- src/lib/content/hero.ts
+++ src/lib/content/hero.ts
@@ -4,7 +4,7 @@ import { resumeFileName } from '@/lib/utils/config'
 export const heroSection: HeroSectionType = {
     subtitle: 'Hi, my name is',
     title: 'Vishal Mohan Nair.',
-    tagline: 'I am Batman',
+    tagline: 'I am a Student',
     description:
     "Hey there! I'm a passionate full-stack developer specializing in creating stunning web applications using React.js, Next.js, TypeScript, and TailwindCSS, ensuring not just functionality but also a delightful user experience. But that's not all—I also dive into the backend world, leveraging Node.js and Express.js to build robust APIs. What sets me apart? My love for AI/ML! From predictive analytics to natural language processing, I'm constantly exploring ways to make computers smarter and integrate AI superpowers into my projects, taking them to the next level. ",
     specialText: 'Currently available for a job & freelance',
